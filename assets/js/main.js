5aa59aa8ac5156604ccbc0565fa8b404785709f1
nirobcoder <emontwo@gmail.com>
2021-07-05 21:47:09 +0600

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
 /**
 * Template Name: MyResume 
-* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
 * Author: Syed Rawson Sadek
-* License: https://bootstrapmade.com/license/
+
 */
 (function() {
   "use strict";
